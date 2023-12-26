9692ccccf44bdc8381a775294782e1f9b925fe0b
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-12-27 01:48:25 +0800

    修复一处错误
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'chat-popup-live',
+  base: '/chat-popup-live/',
   build: {
     rollupOptions: {
       input: {
